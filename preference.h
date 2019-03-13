5f66449c085f72d4a28a1d79898512b3b64b2cbe
KuroiGengetsu <1198148133@qq.com>
2019-03-13 21:22:22 +0800

    add comments

--- preference.h
+++ preference.h
@@ -1,9 +1,9 @@
 // 此文件用于各种预定义
-#define TRUE    1
-#define FALSE   0
-#define OK      1
-#define ERROR   0
-#define INFEASIBLE  -1
-#define OVERFLOW    -2
+#define TRUE    1           // 真
+#define FALSE   0           // 假
+#define OK      1           // OK
+#define ERROR   0           // 错误
+#define INFEASIBLE  -1      // 操作不可行
+#define OVERFLOW    -2      // 溢出
 // Status 是函数类型, 其值是函数结果状态代码
 typedef int Status;
